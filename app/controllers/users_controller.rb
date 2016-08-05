8c86417f349cf21f3d0ffabae21e16a2bbd85040
codeamac15 <codeamac15@gmail.com>
2016-08-05 12:19:57 -0500

    Actions user, to implement index mode

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index 
+    # Tenemos que hacer una variable que contiene
+    # los usuarios de todo el sitio y luego representar cada uno
+    # por iteración a través de ellos en la vista de índice.
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -20,7 +28,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def update
@@ -45,8 +53,15 @@ class UsersController < ApplicationController
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+     # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
